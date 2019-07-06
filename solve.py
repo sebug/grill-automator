f9d094f449ed18f07b6ac1ac90d716b328532ac6
sebug <sebastian.gfeller@gmail.com>
2019-07-06 22:07:38 +0200

    To reasonable array

--- solve.py
+++ solve.py
@@ -15,7 +15,26 @@ def read_screenshot():
     w, h, pixels, metadata = reader.read_flat()
     return (w,h,pixels,metadata)
 
+def pixels_to_tuple_array(w, h, pixels):
+    tuple_array = []
+    pixel_byte_width = 3
+    for y in range(0, h):
+        line_array = []
+        for x in range(0, w):
+            pixel_position = y * w
+            line_array.append(pixels[pixel_position * pixel_byte_width : (pixel_position + 1) * pixel_byte_width])
+        tuple_array.append(line_array)
+    return tuple_array
+
+
+def get_grid(w, h, pixels):
+    print (w, h)
+
 if __name__ == '__main__':
     get_current_screenshot()
-    print read_screenshot()
+    ss = read_screenshot()
+    pixel_array = pixels_to_tuple_array(ss[0], ss[1], ss[2])
+    pix = pixel_array[340][312]
+    print pix[2]
+
 
