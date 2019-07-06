79e3217811de53832ec030269f609f561804ed1f
sebug <sebastian.gfeller@gmail.com>
2019-07-06 21:45:16 +0200

    Reading png

--- solve.py
+++ solve.py
@@ -1,4 +1,5 @@
 import os
+import png, array
 
 adb_path = '~/Library/Android/sdk/platform-tools/adb'
 
@@ -9,7 +10,12 @@ def get_current_screenshot():
     adb_do('shell screencap -p /sdcard/screen.png')
     adb_do('pull /sdcard/screen.png')
 
+def read_screenshot():
+    reader = png.Reader(filename = 'screen.png')
+    w, h, pixels, metadata = reader.read_flat()
+    return (w,h,pixels,metadata)
 
 if __name__ == '__main__':
     get_current_screenshot()
+    print read_screenshot()
 
