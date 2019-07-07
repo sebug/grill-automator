c76fde7131d6de220f893daaa2477bb1d29fb637
sebug <sebastian.gfeller@gmail.com>
2019-07-07 12:27:18 +0200

    Get tile width and height

--- get_grid.py
+++ get_grid.py
@@ -10,6 +10,8 @@ class Playground:
         self.pixels = pixels
         self.metadata = metadata
         self.intersections = False
+        self.tile_width = False
+        self.tile_height = False
 
     def is_black(self, x, y):
         pixel = self.pixel_at(x,y)
@@ -43,6 +45,41 @@ class Playground:
         self.intersections = intersections
         return self.intersections
 
+    def get_tile_width(self):
+        if self.tile_width:
+            return self.tile_width
+        
+        intersections = self.get_intersections()
+        for i in range(0, len(intersections) - 1):
+            (x1, y1) = intersections[i]
+            (x2, y2) = intersections[i + 1]
+            if (x1 != x2 and x1 + 1 != x2):
+                self.tile_width = abs(x1 - x2)
+                return self.tile_width
+        raise ValueError('Expected to have found more than one item in intersections that differ')
+
+    def get_tile_height(self):
+        if self.tile_height:
+            return self.tile_height
+        
+        intersections = self.get_intersections()
+        current_y = -1
+        ys = []
+        for i in range(0, len(intersections)):
+            (x, y) = intersections[i]
+            if current_y != y:
+                current_y = y
+                ys.append(current_y)
+        for i in range(0, len(ys) - 1):
+            current_y = ys[i]
+            next_y = ys[i + 1]
+            if current_y + 1 != next_y:
+                self.tile_height = abs(current_y - next_y) / 2 # this is because the vertical pattern only repeats every two lines
+                return self.tile_height
+            
+        raise ValueError('Expected to have found more than one item in intersections that differ')
+        
+
 def read_screenshot(screenshot_path):
     reader = png.Reader(filename = screenshot_path)
     w, h, pixels, metadata = reader.read_flat()
@@ -51,6 +88,7 @@ def read_screenshot(screenshot_path):
 if __name__ == '__main__':
     screenshot_name = sys.argv[1]
     ss = Playground(screenshot_name)
-    print ss.get_intersections()
-    print ss.get_intersections()
-    print ss.get_intersections()
+    print ss.get_tile_width()
+    print ss.get_tile_width()
+    print ss.get_tile_height()
+    print ss.get_tile_height()
