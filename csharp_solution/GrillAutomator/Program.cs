251af138925f78be95709feda0417871b2c4da54
sebug <sebastian.gfeller@gmail.com>
2019-07-07 23:12:36 +0200

    Trying through CSharp

---
+++ csharp_solution/GrillAutomator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GrillAutomator
+{
+    class MainClass
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length <= 1)
+            {
+                Console.Error.WriteLine("Usage: GrillAutomator image.png tiles_directory");
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
