5d04c6379ce8ec77e2aec31520e5a7cceedec349
sebug <sebastian.gfeller@gmail.com>
2019-07-07 23:15:24 +0200

    Adding params

---
+++ csharp_solution/GrillAutomator/Playground.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace GrillAutomator
+{
+    public class Playground
+    {
+        public string ImagePath { get; }
+        public string TilesPath { get; }
+        public Playground(string imagePath,
+            string tilesPath)
+        {
+            this.ImagePath = imagePath ?? throw new ArgumentNullException(nameof(imagePath));
+            this.TilesPath = tilesPath ?? throw new ArgumentNullException(nameof(tilesPath));
+        }
+
+        public void Detect()
+        {
+
+        }
+    }
+}
--- csharp_solution/GrillAutomator/Program.cs
+++ csharp_solution/GrillAutomator/Program.cs
@@ -11,6 +11,8 @@ namespace GrillAutomator
                 Console.Error.WriteLine("Usage: GrillAutomator image.png tiles_directory");
                 return 1;
             }
+            var playground = new Playground(args[0], args[1]);
+            playground.Detect();
             return 0;
         }
     }
